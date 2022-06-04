718dd0bb1c202dea8876bddcca95a645ecd32385
moonheart <fzp1205561571@hotmail.com>
2022-06-04 23:19:24 +0800

    new code

---
+++ cmd/minimum-depth-of-binary-tree/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return int(math.Min(float64(minDepth(root.Right)), float64(minDepth(root.Left))) + 1)
+}
+
+func main() {
+	node := &TreeNode{
+		Val:  3,
+		Left: &TreeNode{Val: 9},
+		Right: &TreeNode{Val: 20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7}},
+	}
+	println(minDepth(node))
+}
