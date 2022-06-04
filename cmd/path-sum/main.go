596a762a29c17f2c4301c03b41c4a8c34f17c8c4
moonheart <fzp1205561571@hotmail.com>
2022-06-04 23:32:39 +0800

    new code

---
+++ cmd/path-sum/main.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
+
+func main() {
+	node := &TreeNode{Val: -2, Right: &TreeNode{Val: -3}}
+	println(hasPathSum(node, -5))
+}
