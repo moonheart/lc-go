06815cc318322222ed602a7abe54eadd175e69fe
moonheart <fzp1205561571@hotmail.com>
2022-06-18 15:58:16 +0800

    new code

---
+++ cmd/contains-duplicate/main.go
@@ -0,0 +1,12 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	m := map[int]struct{}{}
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
+			return true
+		}
+		m[n] = struct{}{}
+	}
+	return false
+}
