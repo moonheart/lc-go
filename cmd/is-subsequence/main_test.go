6949fad38be88102470cef86795589e3b11105bf
moonheart <fzp1205561571@hotmail.com>
2022-07-29 23:33:11 +0800

    new code

---
+++ cmd/is-subsequence/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func Test_isSubsequence(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{"abc", "ahbgdc"}, true},
+		{"1", args{"axc", "ahbgdc"}, false},
+		{"1", args{"abc", "ahbgadc"}, true},
+		{"1", args{"abc", "ahbgabdc"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSubsequence(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isSubsequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
