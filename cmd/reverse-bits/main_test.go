64e56fc44ec4639c5a2880b7dd3f96f41389efa1
moonheart <fzp1205561571@hotmail.com>
2022-08-08 23:19:40 +0800

    add-binary

---
+++ cmd/reverse-bits/main_test.go
@@ -0,0 +1,24 @@
+package reverse_bits
+
+import "testing"
+
+func Test_reverseBits(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{"1", args{43261596}, 964176192},
+		{"1", args{4294967293}, 3221225471},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBits(tt.args.num); got != tt.want {
+				t.Errorf("reverseBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
