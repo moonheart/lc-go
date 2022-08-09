cbdec988bd9559b8c1c59df1d41ee3c4cf70c9cc
moonheart <fzp1205561571@hotmail.com>
2022-08-09 22:17:49 +0800

    number-of-1-bits

---
+++ cmd/number-of-1-bits/main_test.go
@@ -0,0 +1,25 @@
+package number_of_1_bits
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		n uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{0b00000000000000000000000000001011}, 3},
+		{"1", args{0b00000000000000000000000010000000}, 1},
+		{"1", args{0b11111111111111111111111111111101}, 31},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.n); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
